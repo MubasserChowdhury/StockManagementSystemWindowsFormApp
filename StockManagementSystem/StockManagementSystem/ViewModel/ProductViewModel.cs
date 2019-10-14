40282d86941e62cf0b19fda1664f3cb151386b07
kmhasauiucse <khasan142035@bscse.uiu.ac.bd>
2019-10-14 14:25:22 +0600

    Product Catalog Module (Product)

---
+++ StockManagementSystem/StockManagementSystem/ViewModel/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagementSystem.ViewModel
+{
+    public class ProductViewModel
+    {
+        public int Id { set; get; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int ReorderLevel { get; set; }
+        public string Description { get; set; }
+    }
+}
